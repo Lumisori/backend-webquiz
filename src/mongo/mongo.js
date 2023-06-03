b3874a2e023ebf6f132812e15260ae47fe177516
Eduardo-FCS <eduardofariacaetano100@gmail.com>
2023-06-02 21:48:12 -0300

    Atualização de CRUD

--- src/mongo/mongo.js
+++ src/mongo/mongo.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose');
+const user = '';
+const password = '';
+
 
 const connectDB = () => {
-    try {
-        const dbUrl = 'mongodb+srv://efcs:jdJ41ibzRFyfGXTF@tcc.civo8wn.mongodb.net/?retryWrites=true&w=majority';
-    
-        mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-            .then(() => {
-                console.log('Conectado ao Mongo');
-            })
-            .catch((error) => {
-                console.error('Erro ao conectar ao banco de dados:', error);
-            });
-    } catch (err) {
-        console.log("Erro ao conectar ao banco:" + err)
-    }
+    const dbUrl = `mongodb+srv://${user}:${password}@tcc.civo8wn.mongodb.net/?retryWrites=true&w=majority`;
+
+    mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log('Conectado ao Mongo');
+        })
+        .catch((error) => {
+            console.error('Erro ao conectar ao banco de dados:', error);
+        });
 }
 module.exports = connectDB;
