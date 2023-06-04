ed1111889476efcf18f0e77621e9243c8da0e2a6
Eduardo-FCS <eduardofariacaetano100@gmail.com>
2023-06-04 18:52:13 -0300

    Reorganizando código

--- src/mongo/mongo.js
+++ src/mongo/mongo.js
@@ -4,15 +4,6 @@ const password = 'password';
 
 
 const connectDB = () => {
-    const dbUrl = `mongodb+srv://${user}:${password}@tcc.civo8wn.mongodb.net/?retryWrites=true&w=majority`;
-
-    mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log('Conectado ao Mongo');
-        })
-        .catch((error) => {
-            console.error('Erro ao conectar ao banco de dados:', error);
-        });
     try {
         const dbUrl = `mongodb+srv://${user}:${password}@tcc.civo8wn.mongodb.net/?retryWrites=true&w=majority`;
     
