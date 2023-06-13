e94e31889a3c0caa5c8a197eb2c70002d93ad589
Eduardo-FCS <eduardofariacaetano100@gmail.com>
2023-06-12 21:28:38 -0300

    Adicionando CORS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const connectDB = require('./mongo/mongo');
-const express = require('express')
-const router = require('./router/router')
+const express = require('express');
+const router = require('./router/router');
+const cors = require('cors');
 
 const app = express()
 
+app.use(cors());
 
-app.use(express.json())
-app.use('/quiz', router)
+app.use(express.json());
+app.use('/quiz', router);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 
 try {
     connectDB()
